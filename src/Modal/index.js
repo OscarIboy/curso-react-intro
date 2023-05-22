da196460de923b2d300241038cd4ab7efae5f5fa
Oscar <oscar@r-digital.tech>
2023-05-22 04:48:31 -0600

    set modal

--- src/Modal/index.js
+++ src/Modal/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./Modal.css";
 
-function Modal({children}){
-    return ReactDOM.createPortal(
-        <div className= "Modal">
-            {children}
-        </div>,
+function Modal({ children }) {
+  return ReactDOM.createPortal(
+    <div className="ModalBackground">{children}</div>,
 
-        document.getElementById('modal')
-    );
+    document.getElementById("modal")
+  );
 }
 
-export { Modal }
+export { Modal };
